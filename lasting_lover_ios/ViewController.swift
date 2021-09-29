b275dddfb0107575ded02809c7ff61736c1cc411
Alexey Savchenko <nahalem@ukr.net>
2021-09-29 23:42:50 +0300

    Initial Commit

---
+++ lasting_lover_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lasting_lover_ios
+//
+//  Created by Alexey Savchenko on 29.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
