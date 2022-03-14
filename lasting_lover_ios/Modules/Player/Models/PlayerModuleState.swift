5d0bf6b4a7009ebd446db5846d7a207a4a0d526b
oleksii.savchenko <oleksii.savchenko@pm.bet>
2022-03-14 18:38:59 +0200

    Fixed remote controls

--- lasting_lover_ios/Modules/Player/Models/PlayerModuleState.swift
+++ lasting_lover_ios/Modules/Player/Models/PlayerModuleState.swift
@@ -12,134 +12,138 @@ import AVFoundation
 import MediaPlayer
 
 enum Player {
-  
-  /// sourcery: lens
-  struct State: Hashable {
-    let playbackProgress: Double
-    let isPlaying: Bool
-    let isFavourite: Bool
-    let item: Story
+    
+    /// sourcery: lens
+    struct State: Hashable {
+        let playbackProgress: Double
+        let isPlaying: Bool
+        let isFavourite: Bool
+        let item: Story
         
-    static func `default`(item: Story) -> State {
-      return State(
-        playbackProgress: 0,
-        isPlaying: false,
-				isFavourite: Current.favoritesService().favoriteItems().contains(where: { $0.id == item.id }),
-        item: item
-      )
-    }
-  }
-  
-  /// sourcery: prism
-  enum Action {
-    case playTap
-    case favoriteTap
-    case initializePlayerWithItem
-    case setIsPlaying(value: Bool)
-    case seekToProgress(value: Double)
-    case setPlaybackProgress(value: Double)
-    case fwdSeek
-    case bcwdSeek
-		case forcePausePlayback
-  }
-  
-  static let middleware: Middleware<Player.State, Player.Action> = { dispatch, getState in
-    { next in
-      { action in
-        switch action {
-				case .forcePausePlayback:
-					Current.playerService().pause()
-					
-					let audioSession = AVAudioSession.sharedInstance()
-					do {
-						try audioSession.setCategory(.ambient)
-					}
-					catch {
-						print("Setting category to AVAudioSessionCategoryPlayback failed.")
-					}
-					
-					next(action)
-        case .bcwdSeek:
-          Current.playerService().seekBackward(offset: 15)
-          next(action)
-        case .fwdSeek:
-          Current.playerService().seekForward(offset: 15)
-          next(action)
-        case .setPlaybackProgress(let value):
-          guard !Current.playerService().seeking else { return }
-					
-					guard !value.isNaN, let state = getState() else { return }
-					next(action)
-					
-					if !Current.listentedItemsService().hadListened(state.item.id) {
-						let clamped = value.clamped(min: 0, max: 1)
-						let playbackTime = Double(state.item.audioDuration) * value
-						if playbackTime >= 60 {
-							Current.listentedItemsService().setListened(state.item.id)
-						}
-					}
-					
-        case .seekToProgress(let value):
-          Current.playerService().setPlaybackProgress(value)
-          next(action)
-        case .setIsPlaying:
-          next(action)
-        case .playTap:
-          guard let state = getState() else { return }
-          if state.isPlaying {
-            Current.playerService().pause()
-          } else {
-            Current.playerService().play()
-          }
-          next(action)
-        case .favoriteTap:
-          guard let state = getState() else { return }
-
-					appStore.dispatch(
-						.mainModuleAction(
-							action: .favoritesAction(
-								value: .itemTap(
-									item: state.item
-								)
-							)
-						))
-					next(action)
-				case .initializePlayerWithItem:
-					guard let state = getState() else { return }
-					Current.playerService().setItem(state.item)
-          dispatch(.playTap)
-					
-					try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback, options: .duckOthers)
+        static func `default`(item: Story) -> State {
+            return State(
+                playbackProgress: 0,
+                isPlaying: false,
+                isFavourite: Current.favoritesService().favoriteItems().contains(where: { $0.id == item.id }),
+                item: item
+            )
         }
-      }
     }
-  }
-  
-  static let reducer = Reducer<Player.State, Player.Action> { state, action in
-    switch action {
-    case .setPlaybackProgress(let value):
-			guard !value.isNaN else { return state } 
-			let clamped = value.clamped(min: 0, max: 1)
-      return Player.State.lens.playbackProgress.set(clamped)(state)
-    case .setIsPlaying(let value):
-      return Player.State.lens.isPlaying.set(value)(state)
-    case .favoriteTap:
-      return Player.State.lens.isFavourite.set(!state.isFavourite)(state)
-		case .initializePlayerWithItem, .fwdSeek, .bcwdSeek, .playTap, .seekToProgress, .forcePausePlayback:
-      return state
+    
+    /// sourcery: prism
+    enum Action {
+        case playTap
+        case favoriteTap
+        case initializePlayerWithItem
+        case setIsPlaying(value: Bool)
+        case seekToProgress(value: Double)
+        case setPlaybackProgress(value: Double)
+        case fwdSeek
+        case bcwdSeek
+        case forcePausePlayback
     }
-  }
-  
-  enum Plugin {
-    static let isPlayingPlugin: RxIndependentPlugin<Player.State, Player.Action> = { store in
-      return Current.playerService().isPlaying.bind { value in
-        store?.dispatch(.setIsPlaying(value: value))
-      }
+    
+    static let middleware: Middleware<Player.State, Player.Action> = { dispatch, getState in
+        { next in
+            { action in
+                switch action {
+                case .forcePausePlayback:
+                    Current.playerService().pause()
+                    
+                    let audioSession = AVAudioSession.sharedInstance()
+                    do {
+                        try audioSession.setCategory(.ambient)
+                    }
+                    catch {
+                        print("Setting category to AVAudioSessionCategoryPlayback failed.")
+                    }
+                    
+                    next(action)
+                case .bcwdSeek:
+                    Current.playerService().seekBackward(offset: 15)
+                    next(action)
+                case .fwdSeek:
+                    Current.playerService().seekForward(offset: 15)
+                    next(action)
+                case .setPlaybackProgress(let value):
+                    guard !Current.playerService().seeking else { return }
+                    
+                    guard !value.isNaN, let state = getState() else { return }
+                    next(action)
+                    
+                    if !Current.listentedItemsService().hadListened(state.item.id) {
+                        let clamped = value.clamped(min: 0, max: 1)
+                        let playbackTime = Double(state.item.audioDuration) * value
+                        if playbackTime >= 60 {
+                            Current.listentedItemsService().setListened(state.item.id)
+                        }
+                    }
+                    
+                case .seekToProgress(let value):
+                    Current.playerService().setPlaybackProgress(value)
+                    next(action)
+                case .setIsPlaying:
+                    next(action)
+                case .playTap:
+                    guard let state = getState() else { return }
+                    if state.isPlaying {
+                        Current.playerService().pause()
+                    } else {
+                        Current.playerService().play()
+                    }
+                    next(action)
+                case .favoriteTap:
+                    guard let state = getState() else { return }
+                    
+                    appStore.dispatch(
+                        .mainModuleAction(
+                            action: .favoritesAction(
+                                value: .itemTap(
+                                    item: state.item
+                                )
+                            )
+                        ))
+                    next(action)
+                case .initializePlayerWithItem:
+                    guard let state = getState() else { return }
+                    Current.playerService().setItem(state.item)
+                    dispatch(.playTap)
+                    
+//                    try? AVAudioSession.sharedInstance().setCategory(
+//                        .playback,
+//                        mode: .moviePlayback,
+//                        options: .duckOthers
+//                    )
+                }
+            }
+        }
     }
-    static let playbackProgressPlugin: RxIndependentPlugin<Player.State, Player.Action> = { store in
-      return Current.playerService().playbackProgress.bind { value in
-        store?.dispatch(.setPlaybackProgress(value: value))
-      }
+    
+    static let reducer = Reducer<Player.State, Player.Action> { state, action in
+        switch action {
+        case .setPlaybackProgress(let value):
+            guard !value.isNaN else { return state }
+            let clamped = value.clamped(min: 0, max: 1)
+            return Player.State.lens.playbackProgress.set(clamped)(state)
+        case .setIsPlaying(let value):
+            return Player.State.lens.isPlaying.set(value)(state)
+        case .favoriteTap:
+            return Player.State.lens.isFavourite.set(!state.isFavourite)(state)
+        case .initializePlayerWithItem, .fwdSeek, .bcwdSeek, .playTap, .seekToProgress, .forcePausePlayback:
+            return state
+        }
     }
-  }
+    
+    enum Plugin {
+        static let isPlayingPlugin: RxIndependentPlugin<Player.State, Player.Action> = { store in
+            return Current.playerService().isPlaying.bind { value in
+                store?.dispatch(.setIsPlaying(value: value))
+            }
+        }
+        static let playbackProgressPlugin: RxIndependentPlugin<Player.State, Player.Action> = { store in
+            return Current.playerService().playbackProgress.bind { value in
+                store?.dispatch(.setPlaybackProgress(value: value))
+            }
+        }
+    }
 }
--- lasting_lover_ios/Shared/Services/AudioPlayerService.swift
+++ lasting_lover_ios/Shared/Services/AudioPlayerService.swift
@@ -11,137 +11,134 @@ import RxSwift
 import MediaPlayer
 
 protocol AudioPlayerServiceProtocol {
-  func play()
-  func pause()
-  func setItem(_ item: Story)
-  var playbackProgress: Observable<Double> { get }
-  var isPlaying: Observable<Bool> { get }
-  func setPlaybackProgress(_ value: Double)
-  func seekForward(offset: Double)
-  func seekBackward(offset: Double)
-  var seeking: Bool { get }
-	var currentTimestamp: Double { get }
-	var rate: Float { get }
+    func play()
+    func pause()
+    func setItem(_ item: Story)
+    var playbackProgress: Observable<Double> { get }
+    var isPlaying: Observable<Bool> { get }
+    func setPlaybackProgress(_ value: Double)
+    func seekForward(offset: Double)
+    func seekBackward(offset: Double)
+    var seeking: Bool { get }
+    var currentTimestamp: Double { get }
+    var rate: Float { get }
 }
 
 class AudioPlayerService: AudioPlayerServiceProtocol {
-  
-  static let shared = AudioPlayerService()
     
-  let playbackProgressSubject = PublishSubject<Double>()
-  var playbackProgress: Observable<Double> {
-    return playbackProgressSubject.asObservable()
-  }
-  
-  var isPlaying: Observable<Bool> {
-    return player.rx.observe(Float.self, #keyPath(AVPlayer.rate)).filterNil().map { $0 > 0 }
-  }
-  
-  let player = AVPlayer()
-  
-  private(set) var seeking = false
-	var currentTimestamp: Double = 0
-	var rate: Float {
-		return player.rate
-	}
-  
-  private init() {
-    player.addPeriodicTimeObserver(forInterval: CMTimeMake(value: 1, timescale: 30), queue: nil) { [unowned self] currentTime in
-			self.currentTimestamp = currentTime.seconds
-      if let duration = player.currentItem?.duration.seconds {
-        let progress = currentTime.seconds / duration
-        playbackProgressSubject.onNext(progress)
-      }
+    static let shared = AudioPlayerService()
+    
+    let playbackProgressSubject = PublishSubject<Double>()
+    var playbackProgress: Observable<Double> {
+        return playbackProgressSubject.asObservable()
     }
-  }
-  
-  func seekForward(offset: Double) {
-    let duration = player.currentItem?.duration.seconds ?? 0.0
-    let currentTime = player.currentTime().seconds
-    let targetTime = (currentTime + offset).clamped(min: 0, max: duration)
-    let targetCMTime = CMTimeMakeWithSeconds(targetTime, preferredTimescale: player.currentTime().timescale)
-    player.seek(to: targetCMTime, toleranceBefore: .zero, toleranceAfter: .zero)
-  }
-  
-  func seekBackward(offset: Double) {
-    let duration = player.currentItem?.duration.seconds ?? 0.0
-    let currentTime = player.currentTime().seconds
-    let targetTime = (currentTime - offset).clamped(min: 0, max: duration)
-    let targetCMTime = CMTimeMakeWithSeconds(targetTime, preferredTimescale: player.currentTime().timescale)
-    player.seek(to: targetCMTime, toleranceBefore: .zero, toleranceAfter: .zero)
-  }
-  
-  func play() {
-    player.play()
-  }
-  
-  func pause() {
-    player.pause()
-  }
-  
-  func setItem(_ item: Story) {
-    let plItem = AVPlayerItem(url: item.contentURL)
-		NotificationCenter.default.addObserver(
-			forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
-			object: plItem,
-			queue: nil) { _ in
-				self.setPlaybackProgress(0)
-			}
-    player.replaceCurrentItem(with: plItem)
-		
-		try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: .default, policy: .longFormAudio)
-		try? AVAudioSession.sharedInstance().setActive(true, options: [])
-		
-		DispatchQueue.global().async {
-			let infoCenter = MPNowPlayingInfoCenter.default()
-			
-			var nowPlayingInfo = [String : Any]()
-			nowPlayingInfo[MPMediaItemPropertyTitle] = item.title
-			nowPlayingInfo[MPMediaItemPropertyArtist] = item.author.name
-			nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = CMTimeGetSeconds(self.player.currentTime())
-			nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = plItem.duration.seconds
-			nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = self.player.rate
-			let image: UIImage = Current.imageLoadingService().image(item.artworkURL)
-			nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size) { size in
-				return image
-			}
-
-			infoCenter.nowPlayingInfo = nowPlayingInfo
-		}
-		
-		let commandCenter = MPRemoteCommandCenter.shared()
-		
-		commandCenter.playCommand.isEnabled = true
-		commandCenter.playCommand.addTarget {  event in
-			if self.player.rate == 0 {
-				Current.playerService().play()
-				return .success
-			} else {
-				return .commandFailed
-			}
-		}
-
-		// Add handler for Pause Command
-		commandCenter.pauseCommand.isEnabled = true
-		commandCenter.pauseCommand.addTarget {  event in
-			if self.player.rate != 0 {
-				Current.playerService().pause()
-				return .success
-			} else {
-				return .commandFailed
-			}
-		}
-		
-  }
-  
-  func setPlaybackProgress(_ value: Double) {
-    guard !seeking else { return }
-    seeking = true
-    if let duration = player.currentItem?.duration {
-      let result = CMTimeMultiplyByFloat64(duration, multiplier: value)
-      player.seek(to: result, toleranceBefore: .zero, toleranceAfter: .zero) { finished in
-        self.seeking = false
-      }
+    
+    var isPlaying: Observable<Bool> {
+        return player.rx.observe(Float.self, #keyPath(AVPlayer.rate)).filterNil().map { $0 > 0 }
     }
-  }
+    
+    let player = AVPlayer()
+    
+    private(set) var seeking = false
+    var currentTimestamp: Double = 0
+    var rate: Float {
+        return player.rate
+    }
+    
+    private init() {
+        player.addPeriodicTimeObserver(forInterval: CMTimeMake(value: 1, timescale: 30), queue: nil) { [unowned self] currentTime in
+            self.currentTimestamp = currentTime.seconds
+            if let duration = player.currentItem?.duration.seconds {
+                let progress = currentTime.seconds / duration
+                playbackProgressSubject.onNext(progress)
+            }
+        }
+    }
+    
+    func seekForward(offset: Double) {
+        let duration = player.currentItem?.duration.seconds ?? 0.0
+        let currentTime = player.currentTime().seconds
+        let targetTime = (currentTime + offset).clamped(min: 0, max: duration)
+        let targetCMTime = CMTimeMakeWithSeconds(targetTime, preferredTimescale: player.currentTime().timescale)
+        player.seek(to: targetCMTime, toleranceBefore: .zero, toleranceAfter: .zero)
+    }
+    
+    func seekBackward(offset: Double) {
+        let duration = player.currentItem?.duration.seconds ?? 0.0
+        let currentTime = player.currentTime().seconds
+        let targetTime = (currentTime - offset).clamped(min: 0, max: duration)
+        let targetCMTime = CMTimeMakeWithSeconds(targetTime, preferredTimescale: player.currentTime().timescale)
+        player.seek(to: targetCMTime, toleranceBefore: .zero, toleranceAfter: .zero)
+    }
+    
+    func play() {
+        player.play()
+    }
+    
+    func pause() {
+        player.pause()
+    }
+    
+    func setItem(_ item: Story) {
+        let plItem = AVPlayerItem(url: item.contentURL)
+        NotificationCenter.default.addObserver(
+            forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+            object: plItem,
+            queue: nil) { _ in
+                self.setPlaybackProgress(0)
+            }
+        player.replaceCurrentItem(with: plItem)
+        
+        try? AVAudioSession.sharedInstance()
+            .setCategory(
+                .playback,
+                mode: .default,
+                policy: .longFormAudio
+            )
+        try? AVAudioSession.sharedInstance().setActive(true, options: [])
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = [
+            MPMediaItemPropertyTitle: item.title,
+            MPMediaItemPropertyArtist: item.author.name,
+            MPNowPlayingInfoPropertyElapsedPlaybackTime: CMTimeGetSeconds(self.player.currentTime()),
+            MPMediaItemPropertyPlaybackDuration: plItem.duration.seconds,
+            MPNowPlayingInfoPropertyPlaybackRate: self.player.rate,
+            MPMediaItemPropertyArtwork: MPMediaItemArtwork(boundsSize: CGSize(width: 200, height: 200)) { size in
+                return Current.imageLoadingService().image(item.artworkURL)
+            }
+        ]
+        
+        setupNowPlayingInfoCenter()
+    }
+    
+    func setupNowPlayingInfoCenter(){
+        UIApplication.shared.beginReceivingRemoteControlEvents()
+        MPRemoteCommandCenter.shared().playCommand.addTarget {event in
+          self.play()
+          return .success
+        }
+        MPRemoteCommandCenter.shared().pauseCommand.addTarget {event in
+          self.pause()
+          return .success
+        }
+        MPRemoteCommandCenter.shared().nextTrackCommand.addTarget {event in
+//          self.goForward()
+          return .success
+        }
+        MPRemoteCommandCenter.shared().previousTrackCommand.addTarget {event in
+//          self.goBackward()
+          return .success
+        }
+      }
+    
+    func setPlaybackProgress(_ value: Double) {
+        guard !seeking else { return }
+        seeking = true
+        if let duration = player.currentItem?.duration {
+            let result = CMTimeMultiplyByFloat64(duration, multiplier: value)
+            player.seek(to: result, toleranceBefore: .zero, toleranceAfter: .zero) { finished in
+                self.seeking = false
+            }
+        }
+    }
 }
